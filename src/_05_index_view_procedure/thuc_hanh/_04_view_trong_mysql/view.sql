7785a01fa3d835e2405ac27b2f7c0cefc7f2ddcd
MangDunh <md0949470893@gmail.com>
2021-06-16 14:36:45 +0700

    Bài 5: View, Index and Stored Procedure - Bài thực hành 4: View trong MySQL

---
+++ src/_05_index_view_procedure/thuc_hanh/_04_view_trong_mysql/view.sql
@@ -0,0 +1,26 @@
+-- [Thực hành] View trong MySql
+Use classicmodels;
+CREATE VIEW customer_views AS
+    SELECT 
+        customerNumber, customerName, phone
+    FROM
+        customers;
+                                
+SELECT 
+    *
+FROM
+    customer_views;
+
+CREATE OR REPLACE VIEW customer_views AS
+    SELECT 
+        customerNumber,
+        customerName,
+        contactFirstName,
+        contactLastName,
+        phone
+    FROM
+        customers
+    WHERE
+        city = 'Nantes';
+
+DROP VIEW customer_views;
