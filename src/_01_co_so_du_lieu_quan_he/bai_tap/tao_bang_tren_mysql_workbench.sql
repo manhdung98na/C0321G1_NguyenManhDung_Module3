0f1eda48e656e94064728f37eb9b3cbdfe575c64
MangDunh <md0949470893@gmail.com>
2021-06-16 11:14:03 +0700

    Bài 1: Cơ sở dữ liệu quan hệ - Bài tập 1: Xây dựng CSDL quản lý sinh vien

--- src/_01_co_so_du_lieu_quan_he/bai_tap/tao_bang_tren_mysql_workbench.sql
+++ src/_01_co_so_du_lieu_quan_he/bai_tap/tao_bang_tren_mysql_workbench.sql
@@ -1,8 +1,20 @@
 create database student_management;
 use student_management;
 Create table student(
-	id int not null primary key,
-    name varchar(45) null,
-    age int null,
-    country varchar(45) null
+	`id` int not null primary key,
+    `name` varchar(45) null,
+    `age` int null,
+    `country` varchar(45) null
+);
+
+Create table class(
+	`id` int not null primary key,
+    `name` varchar(45)
+);
+
+Create table teacher(
+	`id` int not null primary key,
+    `name` varchar(45),
+    `age` int,
+    `country` varchar(45)
 );
