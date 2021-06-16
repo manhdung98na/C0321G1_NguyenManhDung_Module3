dc871ba8e361c9b9825ca64986da60c5cd5f33cc
MangDunh <md0949470893@gmail.com>
2021-06-16 08:30:31 +0700

    Bài 5: SQL, View, Index and Stored Procedure - Bài thực hành 1: Chỉ mục trong MySQL

---
+++ src/_05_index_view_procedure/thuc_hanh/chi_muc_trong_sql.sql
@@ -0,0 +1,15 @@
+-- [Thực hành] Chỉ mục trong MySql
+use classicmodels;
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    customerName = 'Land of Toys Inc.'; 
+	
+EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+ALTER TABLE customers ADD INDEX idx_customerName(customerName);
+    
+ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
+EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' OR contactFirstName = 'King';
+    
