2a506493f059769b2eebe18537e3aa1cc0cb9902
manhdung98na <md0949470893@gmail.com@github.com>
2021-07-04 12:05:52 +0700

    Bài 13 - Bài tập 2: Luyện tập sử dụng Transaction

---
+++ src/_13_jdbc_querying_and_transaction/bai_tap/_02_luyen_tap_transaction/src/main/java/model/service/UserService.java
@@ -0,0 +1,39 @@
+package model.service;
+
+import model.bean.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserService {
+    public void insertUser(User user) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User user) throws SQLException;
+
+    public List<User> searchByCountry(String country);
+
+    User getUserById(int id);
+
+    void insertUserStore(User user) throws SQLException;
+
+    void addUserTransaction(User user, int[] permisions);
+
+    public void insertUpdateWithoutTransaction();
+
+    public void insertUpdateUseTransaction();
+
+    public List<User> selectAllUsersByProcedure() throws SQLException;
+
+    public boolean updateUserByProcedure(User user) throws SQLException;
+
+    public boolean deleteUserByProcedure(int id) throws SQLException;
+
+    public void addToUserTableTransaction(User[] users);
+
+}
---
+++ src/_13_jdbc_querying_and_transaction/bai_tap/_02_luyen_tap_transaction/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 1/7/2021
+  Time: 1:20 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body onload="chuyenHuong()">
+$END$
+</body>
+<script>
+    function chuyenHuong() {
+        location.href = "/users"
+    }
+</script>
+</html>
